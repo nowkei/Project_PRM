f22913562bc4b06151d574cb48ab554b3976f478
nowkei <hongthang394@gmail.com>
2023-10-14 19:47:59 +0700

    Update theme, call back logic and lib for project

--- app/src/main/java/com/example/project_prm/fragment/login/LoginCallback.java
+++ app/src/main/java/com/example/project_prm/fragment/login/LoginCallback.java
@@ -3,5 +3,5 @@ package com.example.project_prm.fragment.login;
 public interface LoginCallback {
     void onLoginResult(boolean result, String message);
 
-    void onSomethingResult();
+    void onLoading(boolean isLoading);
 }
