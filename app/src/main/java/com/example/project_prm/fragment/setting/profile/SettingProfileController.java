2973e4541cd438c672f103b60a40d0d9fe8c87ef
nowkei <hongthang394@gmail.com>
2023-11-03 23:56:24 +0700

    get user profile in SettingProfileFragment in database

---
+++ app/src/main/java/com/example/project_prm/fragment/setting/profile/SettingProfileController.java
@@ -0,0 +1,51 @@
+package com.example.project_prm.fragment.setting.profile;
+
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.example.project_prm.model.User;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SettingProfileController {
+
+    private SettingProfileCallback settingProfileCallback;
+
+    private DatabaseReference databaseReferences;
+
+    public SettingProfileController(SettingProfileCallback settingProfileCallback) {
+        this.settingProfileCallback = settingProfileCallback;
+    }
+
+    public void getUserProfile(String uid) {
+        settingProfileCallback.onLoading(true);
+        databaseReferences = FirebaseDatabase.getInstance().getReference();
+        databaseReferences.child("Users").child(uid).child("info").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                if (task.isSuccessful()) {
+                    User user = new User();
+                    Map<String,String> data = (HashMap<String, String>) task.getResult().getValue();
+                    user.setUid(uid);
+                    user.setAddress(data.get("address"));
+                    user.setEmail(data.get("email"));
+                    user.setPhoneNumber(data.get("phoneNumber"));
+                    user.setPassword(data.get("password"));
+                    user.setUsername(data.get("username"));
+
+                    settingProfileCallback.onGetUserResult(true, "Get user profile success", user);
+                } else {
+                    settingProfileCallback.onGetUserResult(false, "Login fail, please try again", null);
+                }
+                settingProfileCallback.onLoading(false);
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/project_prm/model/User.java
+++ app/src/main/java/com/example/project_prm/model/User.java
@@ -2,13 +2,32 @@ package com.example.project_prm.model;
 
 public class User {
 
+    private String uid;
     private String username;
     private String password;
     private String email;
     private String phoneNumber;
+    private String avatar;
+    private String address;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 
     public User() {
 
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
     }
 
     public User(String username, String password) {
@@ -17,6 +36,29 @@ public class User {
     }
 
 
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 
     public String getPassword() {
         return password;
